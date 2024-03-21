df83c2696e3bc5dfa2e7b0d193c15537c6ac7936
Aningwu Chisom <aningwuchisom@gmail.com>
2024-03-21 20:02:50 +0100

    finishing up with the task

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,57 +1,115 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-function ToDoList(){
+function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-    const[tasks, setTasks] = useState([" Say A Pray", "Exercise", "eat Breakfast"]);
-    const [newTask, setNewTask] = useState("");
+  const [editItemTask, setEditItemTask] = useState("");
+  const [editTaskIndex, setEditTaskIndex] = useState(null);
 
-    function handleInputChange(event){
-        setNewTask(event.target.value);
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function setEditItemTaskChange(event) {
+    setEditItemTask(event.target.value);
+  }
+
+  function setEditTaskIndexFunc(index) {
+    setEditTaskIndex(index);
+    setEditItemTask(tasks[index]);
+  }
+
+  function editTask(indexOfItem) {
+    if (editItemTask.trim() !== "") {
+      setTasks(
+        tasks.map((item, index) =>
+          index === indexOfItem ? editItemTask : item
+        )
+      );
+      setEditItemTask("");
+      setEditTaskIndex(null);
     }
+  }
 
-    function addTask(){
-
-        if(newTask.trim() !==""){
-            setTasks(t => [...t, newTask]);
-            setNewTask("");
-        }
-   
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
     }
+  }
 
-    function deleteTask(index){
-      
-    } 
+  function deleteTask(indexOfItem) {
+    setTasks(tasks.filter((_, index) => indexOfItem !== index));
+  }
 
-    function editTask(index){
+  return (
+    <div className="to-do-list">
+      <h1>
+        TO-DO-List <span>APP</span>{" "}
+      </h1>
 
-    }
-return(<div className="to-do-list">
+      <div>
+        <input
+          type="text"
+          placeholder="Add Task...."
+          value={newTask}
+          onChange={handleInputChange}
+        />
 
-<h1>TO-DO-List <span>APP</span>  </h1>
-
-    <div>
-        <input type = "text" placeholder="Add Task...." value = {newTask} onChange= {handleInputChange}/>
-
-        <button className='add-btn' onClick={addTask}>
-            Add
+        <button className="add-btn" onClick={addTask}>
+          Add
         </button>
+      </div>
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {editTaskIndex !== index ? (
+              <div
+                style={{
+                  flex: "1",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <div className="list-text">{task}</div>
+
+                <div>
+                  <button
+                    className="delete-btn"
+                    onClick={() => deleteTask(index)}
+                  >
+                    Delete
+                  </button>
+
+                  <button
+                    className="edit-btn"
+                    onClick={() => setEditTaskIndexFunc(index)}
+                  >
+                    ✏️
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <div style={{ display: "flex", width: "100%" }}>
+                <input
+                  style={{ flex: "1", minWidth: "10px" }}
+                  type="text"
+                  placeholder="Edit Task...."
+                  value={editItemTask}
+                  onChange={setEditItemTaskChange}
+                />
+
+                <button className="add-btn" onClick={() => editTask(index)}>
+                  Save
+                </button>
+              </div>
+            )}
+          </li>
+        ))}
+      </ol>
     </div>
-<ol>
-    {tasks.map((task, index) => 
-    <li key={index}>
-        <span className = "list-text">{task}</span>
-
-        <button className="delete-btn" onClick={deleteTask(() => index)}>
-            Delete 
-        </button>
-
-        
-        <button className="edit-btn" onClick={ editTask(() => index)}>
-             ✏️
-        </button>
-    </li>)}
-</ol>
-</div>);
-
+  );
 }
-export default ToDoList
+export default ToDoList;
